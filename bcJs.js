c8a4514e23740d46a7893c0c41c0503a1ead6a16
IAMYSJ <s1003352@gmail.com>
2017-07-03 22:16:04 +0800

    Add files via upload

--- bcJs.js
+++ bcJs.js
@@ -1,19 +1,38 @@
 $(document).ready(function() {
-	answerNum = Math.floor(Math.random()*10000);
-	var guessNum = document.getElementById('guessNum');
-	var guessTime = 1;
-	if(answerNum < 1000) {
-		answerNum = answerNum*10 + Math.floor(Math.random()*10);
+	/*
+		亂數產生4位不重複的數字
+	*/
+	var allNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+	answerNumArray = [];
+	for(var i = 0; i < 4; i++) {
+		var getNum = Math.floor(Math.random()*allNum.length);
+		answerNumArray[i] = allNum[getNum];
+		allNum.splice(getNum, 1);
 	}
-
-	// 開始猜數字
+	var answerNum = answerNumArray[0]*1000 + answerNumArray[1]*100 + answerNumArray[2]*10 + answerNumArray[3];
+	console.log(answerNumArray);
+	/* 
+		開始猜數字
+	*/
+	var guessTime = 1;
 	$('#confirmBtn').click(function() {
+		// 1. 取得玩家輸入的數字
+		// 2. 拆成千位、百位、十位、個位
+		var guessNum = document.getElementById('guessNum').value;
+		guess1 = Math.floor(guessNum/1000);
+		guess2 = Math.floor((guessNum - guess1*1000)/100);
+		guess3 = Math.floor((guessNum - guess1*1000 - guess2*100)/10);
+		guess4 = Math.floor(guessNum - guess1*1000 - guess2*100 - guess3*10);
+		console.log(guess1);
+		console.log(guess2);
+		console.log(guess3);
+		console.log(guess4);
 		if(guessTime < 9) {
-			if(guessNum.value > 1000 && guessNum.value < 9999) {
-				if(parseInt(guessNum.value) != answerNum) {
-					$('#content').append("<tr><td>" + guessTime + "</td><td>" + guessNum.value + "</td><td>" + numHint(parseInt(guessNum.value)) + "</td></tr>");
+			if(guessNum.length >= 4) {
+				if(guessNum != answerNum) {
+					$('#content').append("<tr><td>" + guessTime + "</td><td>" + guessNum + "</td><td>" + numHint() + "</td></tr>");
 				} else {
-					$('#content').append("<tr><td>" + guessTime + "</td><td>" + guessNum.value + "</td><td>" + numHint(parseInt(guessNum.value)) + "</td></tr>");
+					$('#content').append("<tr><td>" + guessTime + "</td><td>" + guessNum + "</td><td>" + numHint() + "</td></tr>");
 					alert('Congratulations!');
 					$('#confirmBtn').prop('disabled', true);
 					$('#restartBtn').css('display', 'inline');
@@ -23,55 +42,80 @@ $(document).ready(function() {
 				alert('Please enter a 4-digits number!');
 			}
 		} else {
-			$('#content').append("<tr><td>" + guessTime + "</td><td>" + guessNum.value + "</td><td>" + numHint(parseInt(guessNum.value)) + "</td></tr>");
-			$('#content').append("<tr><td>Answer</td><td>" + answerNum + "</td>");
+			$('#content').append("<tr><td>" + guessTime + "</td><td>" + guessNum + "</td><td>" + numHint() + "</td></tr>");
+			$('#content').append("<tr><td>Answer</td><td>" + answerMum + "</td>");
 			$('#confirmBtn').prop('disabled', true);
 			$('#restartBtn').css('display', 'inline');
 		}
 	});
-	
 	// 重新開始
 	$('#restartBtn').click(function() {
 		location.reload();
 	});
 });
-
-function numHint(guessNum) {
-	countA = 0;
-	countB = 0;
-	
-	var guessNum1 = Math.floor(guessNum/1000);
-	var guessNum2 = Math.floor((guessNum - 1000*guessNum1)/100);
-	var guessNum3 = Math.floor((guessNum - 1000*guessNum1 - 100*guessNum2)/10);
-	var guessNum4 = Math.floor(guessNum - 1000*guessNum1 - 100*guessNum2 - 10*guessNum3);
-	
-	var answerNum1 = Math.floor(answerNum/1000);
-	var answerNum2 = Math.floor((answerNum - 1000*answerNum1)/100);
-	var answerNum3 = Math.floor((answerNum - 1000*answerNum1 - 100*answerNum2)/10);
-	var answerNum4 = Math.floor(answerNum - 1000*answerNum1 - 100*answerNum2 - 10*answerNum3);
-	
-	abHint(guessNum1, answerNum1, answerNum2, answerNum3, answerNum4);
-	abHint(guessNum2, answerNum2, answerNum1, answerNum3, answerNum4);
-	abHint(guessNum3, answerNum3, answerNum1, answerNum3, answerNum4);
-	abHint(guessNum4, answerNum4, answerNum1, answerNum3, answerNum4);
-
-	var result = countA + 'A' + countB + 'B';
-
-	return result;
-}
-
-function abHint(guessNum, answerNum1, answerNum2, answerNum3, answerNum4) {
-	if(guessNum == answerNum1) {
-		countA++;
-	} else {
-		if(guessNum == answerNum2) {
+/*
+	判斷幾A幾B
+*/
+function numHint() {
+	var countA = 0;
+	var countB = 0;
+	switch(guess1) {
+		case answerNumArray[0]:
+			countA++;
+			break;
+		case answerNumArray[1]:
 			countB++;
-		}
-		if(guessNum == answerNum3) {
+			break;
+		case answerNumArray[2]:
 			countB++;
-		}
-		if(guessNum == answerNum4) {
+			break;
+		case answerNumArray[3]:
 			countB++;
-		}
+			break;
 	}
+	switch(guess2) {
+		case answerNumArray[0]:
+			countB++;
+			break;
+		case answerNumArray[1]:
+			countA++;
+			break;
+		case answerNumArray[2]:
+			countB++;
+			break;
+		case answerNumArray[3]:
+			countB++;
+			break;
+	}
+	switch(guess3) {
+		case answerNumArray[0]:
+			countB++;
+			break;
+		case answerNumArray[1]:
+			countB++;
+			break;
+		case answerNumArray[2]:
+			countA++;
+			break;
+		case answerNumArray[3]:
+			countB++;
+			break;
+	}
+	switch(guess4) {
+		case answerNumArray[0]:
+			countB++;
+			break;
+		case answerNumArray[1]:
+			countB++;
+			break;
+		case answerNumArray[2]:
+			countB++;
+			break;
+		case answerNumArray[3]:
+			countA++;
+			break;
+	}
+	console.log(countA);
+	console.log(countB);
+	return countA + "A" + countB + "B";
 }
